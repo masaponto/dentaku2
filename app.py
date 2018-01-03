cf31a4f6ecf7fa4b9a6a182053af3927c0a45782
masaponto <masaponto@gmail.com>
2018-01-04 01:19:01 +0900

    [Add] python server

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 import tensorflow as tf
 import numpy as np
 from mnist import model
+from flask import Flask, render_template, request, jsonify
 
+app = Flask(__name__, template_folder='build', static_folder='build')
 x = tf.placeholder("float", [None, 784])
 sess = tf.Session()
 
@@ -19,6 +21,30 @@ def cnn(input_x):
     return sess.run(y_conv2, feed_dict={x: input_x, keep_prob: 1.0}).flatten().tolist()
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#@app.route('/estimate', methods=["POST"])
+# def estimate():
+#    try:
+#        input_x = (np.array(request.json["input"],
+#                            dtype=np.uint8) / 255.0).reshape(1, 784)
+#        output = convolutional(input_x)
+#
+#        return jsonify({"estimated": output})
+#
+#    except Exception as e:
+#        print(e)
+#        return jsonify({"error": e})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
+
+
+'''
 def main():
     from tensorflow.examples.tutorials.mnist import input_data
 
@@ -27,7 +53,4 @@ def main():
     label = mnist.train.labels[0].tolist()
     y = cnn(input_x.reshape(1, 784))
     print(y.index(max(y)), label.index(max(label)))
-
-
-if __name__ == "__main__":
-    main()
+'''
